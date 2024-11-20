63ba0deeaf581f28c9fde1b52b7c3e1b28fa574c
ViniciusDev <viniciusdev10@gmail.com>
2024-11-20 10:42:41 -0300

    created project

---
+++ src/main/java/com/devvinicius/backendpicpay/BackendpicpayApplication.java
@@ -0,0 +1,13 @@
+package com.devvinicius.backendpicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendpicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendpicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devvinicius/backendpicpay/BackendpicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devvinicius.backendpicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendpicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
